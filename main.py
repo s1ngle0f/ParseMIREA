294bbe5fa6277d323b0c579600a5ecbbb35e8c15
Zubkov Mihail <zubkovmihail12345@gmail.com>
2022-11-17 20:00:50 +0300

    Exe file

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import write_to_template
-
+import json
 
 if __name__ == '__main__':
-    # write_to_template.write_all_prepods()
-    write_to_template.write_all_auditories()
-    # write_to_template.write_all_lessons()
+    with open('settings.json') as f:
+        settings = json.load(f)
+        if settings['prepods']:
+            write_to_template.write_all_prepods()
+            # print('prepods')
+        if settings['auditories']:
+            write_to_template.write_all_auditories()
+            # print('auditories')
+        if settings['lessons']:
+            write_to_template.write_all_lessons()
+            # print('lessons')
 
